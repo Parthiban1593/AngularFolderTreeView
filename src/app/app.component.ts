1cd87daa6ef65cfa203761b35e08e1aec387ebd6
Parthiban1593 <parthibanbe93@gmail.com>
2024-04-10 17:49:42 +0530

    Search Implementation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,6 +5,7 @@ import { AddFolderPopupComponent } from './popup/add-folder-popup/add-folder-pop
 import {MatDialog, MatDialogRef, MAT_DIALOG_DATA} from '@angular/material/dialog'
 import { ChangeDetectionStrategy } from '@angular/compiler';
 import { MatMenu, MatMenuTrigger } from '@angular/material/menu';
+import { FormControl } from '@angular/forms';
 /**
  * Food data with nested structure.
  * Each node has a name and an optiona list of children.
@@ -69,12 +70,47 @@ export class AppComponent {
   myFoldersTreeControl = new NestedTreeControl<FolderNode>(node => node.children);
   myFoldersDataSource = new ArrayDataSource(this.myFoldersHierachy);
   contextMenuPosition = { x: '0px', y: '0px' };
+  searchFolderOrNode : FormControl = new FormControl();
 
   hasChild = (_: number, node: FolderNode) => !!node.children && node.children.length > 0;
   title = 'FolderStructureDemo';
 
   constructor(public dialog: MatDialog,private cdr : ChangeDetectorRef) {}
 
+  ngAfterViewInit(){
+    this.searchFolderOrNode.valueChanges.subscribe((searchString)=>{
+      let shareWithMeFilteredArr = [];
+      let myFolderArr=[]
+      if(searchString.length > 3){
+        shareWithMeFilteredArr = this.filterFolderHierarchy(this.folderHierarchy,searchString)
+        this.dataSource = new ArrayDataSource(shareWithMeFilteredArr);
+        myFolderArr = this.filterFolderHierarchy(this.myFoldersHierachy,searchString)
+        this.myFoldersDataSource = new ArrayDataSource(myFolderArr);
+      }else{
+        this.dataSource = new ArrayDataSource(this.folderHierarchy);
+        this.myFoldersDataSource = new ArrayDataSource(this.myFoldersHierachy);
+      }
+    })
+  }
+
+  filterFolderHierarchy(folderArray: FolderNode[], searchTerm: string): FolderNode[] {
+    return folderArray.reduce((filtered: FolderNode[], current: FolderNode) => {
+      // Check if the current node matches the search term
+      if (current.name.toLowerCase().includes(searchTerm.toLowerCase())) {
+        // If it matches, add it to the filtered array
+        filtered.push(current);
+      } else if (current.children) {
+        // If it doesn't match and has children, recursively filter its children
+        const filteredChildren = this.filterFolderHierarchy(current.children, searchTerm);
+        // If any child matches, add the current node to the filtered array
+        if (filteredChildren.length > 0) {
+          filtered.push({ ...current, children: filteredChildren });
+        }
+      }
+      return filtered;
+    }, []);
+  }
+  
   addNewFolder(): void {
     const dialogRef = this.dialog.open(AddFolderPopupComponent, {
       width: '450px',
